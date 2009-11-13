6feff3afa923345fbc2d2d89d82aedbedae07644
Herve CUCHE <herve.cuche@gmail.com>
2009-11-13 19:24:17 +0100

    Add working svm and kernel (polynomial, RBF) Just one problem, I don't use the bias...

--- classification/svm.py
+++ classification/svm.py
@@ -15,17 +15,21 @@ from cvxopt.base import matrix
 
 max_xy = 10 # Max spread
 
-
 if len(sys.argv) >= 2:
     filename = sys.argv[1]
 
 def linear_k(x, y):
-    return numpy.dot(numpy.transpose(x), y)
-
+    val = numpy.sum(numpy.dot(x,y), dtype=float) + 1
+    return val
 
 def polynomial_k(x, y):
-    a = (numpy.dot(numpy.transpose(x), y)) ** int(p_value)
-    return a
+    val = numpy.power((numpy.sum(numpy.dot(x,y), dtype=float) + 1), int(p_value))
+    return val
+
+def rbf_k(x, y):
+    m = numpy.linalg.norm(numpy.power((x - y), 2))
+    val = numpy.exp(-1 * m * float(p_value))
+    return val
 
 test = 0
 p_value = 1
@@ -34,12 +38,15 @@ if sys.argv[-1] == '--test':
 if sys.argv[-3] == '--poly_k':
     kernel = polynomial_k
     p_value = sys.argv[-2]
+if sys.argv[-3] == '--rbf_k':
+    kernel = rbf_k
+    p_value = sys.argv[-2]
 if sys.argv[-2] == '--linear_k':
     kernel = linear_k
 
 
 class SVM(object):
-    def __init__(self, kernel=linear_k):
+    def __init__(self):
         self.kernel = kernel
         self.alpha = []
         self.bias = 0.
@@ -47,9 +54,10 @@ class SVM(object):
 
 
     def train(self, data, labels):
+        self.data = data
+        self.labels = labels
         self.alpha = self.lagrange_coeffs(data, labels)
         self.bias = self.bias_get(data, labels)
-        print self.bias
 
     # Get weight vector
     def __weight(self, data, labels):
@@ -62,7 +70,10 @@ class SVM(object):
     def decision(self, data):
         deci = []
         for d in data:
-            deci.append(numpy.dot(numpy.transpose(self.weight), d) + self.bias)
+            w = 0.
+            for i in xrange(0, len(self.alpha)):
+                w += self.alpha[i][0] * self.labels[self.alpha[i][1]] * self.kernel(self.data[self.alpha[i][1]], d)
+            deci.append(w)
         return deci
 
     # Get new label
@@ -77,33 +88,37 @@ class SVM(object):
                 new_lab.append(-1)
         return new_lab
 
-
     # get bias of svm (page 12-14)
     def bias_get(self, data, labels):
-        self.weight = self.__weight(data, labels)
+#         self.weight = self.__weight(data, labels)
 
-        b = 0.
-        for i in xrange(0, len(self.alpha)):
-            b += (labels[self.alpha[i][1]] - numpy.dot(numpy.transpose(self.weight), data[self.alpha[i][1]]))
+#         b = 0.
+#         for i in xrange(0, len(self.alpha)):
+# #             for j in xrange(0, len(self.alpha)):
+# #                 k = self.kernel(data[self.alpha[i][1]], data[self.alpha[j][1]])
+# #                 print k, self.weight
+#             b += (labels[self.alpha[i][1]] - self.kernel(self.weight, data[self.alpha[i][1]]))
 
-        return b / len(self.alpha)
-
+#         return b / len(self.alpha)
+        return
 
     # Get the matrice P for the solver
     def __get_quad(self, data, n, l):
         p = (n,n)
         P = numpy.zeros(p)
         for i in xrange(0, n):
-            for j in xrange(0, n):
+            for j in xrange(i, n):
                 # (see Annexe page 3 formula 8)
-                P[i, j] = l[i] * l[j] * self.kernel(data[i], data[j])
+                val = l[i] * l[j] * self.kernel(data[i], data[j])
+                P[i, j] = val
+                P[j, i] = val
         return P
 
     # get lagrange coefficient (alpha vector)
     # use annexe to understand
     def lagrange_coeffs(self, data, labels):
         n = len(labels)
-        q = numpy.dot (numpy.ones(n), -1)
+        q = numpy.dot(numpy.ones(n), -1)
         h = numpy.zeros(n)
         G = numpy.dot(numpy.eye(n), -1)
         P = self.__get_quad(data, n, labels)
@@ -119,25 +134,29 @@ class SVM(object):
         alpha = [(int(al[i]), i)  for i in xrange(0, len(al)) if al[i] <> 0]
         return alpha
 
-    def print_2Ddecision(self, bounds, print_sv=True, print_non_sv=False):
+    def print_2Ddecision(self, data, lab, bounds, print_sv=True, print_non_sv=False):
+        i = 0
+        pylab.figure()
+
+        for i in xrange(0, len(lab)):
+            if lab[i] >= 0:
+                pylab.scatter(data[i][0], data[i][1], c="r", marker='o')
+            else:
+                pylab.scatter(data[i][0], data[i][1], c="b", marker='o')
+
+        X = []
+        Y = []
+        for a in self.alpha:
+            X.append(data[a[1]][0])
+            Y.append(data[a[1]][1])
+            pylab.scatter(X, Y, s=200, c="r", marker='o')
+
+        pylab.axis([-max_xy - 1, max_xy + 1, -max_xy - 1, max_xy + 1])
+        pylab.savefig(filename + ".pdf")
+        pylab.savefig(filename + ".eps")
+        pylab.savefig(filename + ".svg")
+
         return
-
-def display(data, labels):
-    i = 0
-    pylab.figure()
-    for d in data:
-        if labels[i] == -1:
-            pylab.scatter([d[0]], [d[1]], c="r", marker='^')
-        else:
-            pylab.scatter([d[0]], [d[1]], c="b", marker='o')
-        i = i + 1
-
-    pylab.axis([-max_xy - 1, max_xy + 1, -max_xy - 1, max_xy + 1])
-    pylab.savefig(filename + ".pdf")
-    pylab.savefig(filename + ".eps")
-    pylab.savefig(filename + ".svg")
-
-print "Generating " + filename
 
 def __test():
     print "Testing SVM..."
@@ -153,11 +172,10 @@ def __test():
 #     d = numpy.array([[1], [2], [3]])
 #     l = [-1.0, 1.0, 1.0]
 
-    svm = SVM(polynomial_k);
+    svm = SVM();
     svm.train(data, l)
     lab = svm.process(data)
 
-    display(data, lab)
-
+    svm.print_2Ddecision(data, lab, 1)
 if test:
     __test()
